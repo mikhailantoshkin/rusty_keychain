1528e5caf1d0ce52195d5b8fde6525e74d695a11
mikhailantoshkin <mike.antoshkin@gmail.com>
2021-03-13 23:18:09 +0500

    added rudimentary ctrl+c hook
    otherwise user will lose passwords added in current session

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::string::String;
 
+use ctrlc;
 use rusty_keychain::KeyChain;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 fn main() {
     println!("Enter master pass");
@@ -9,7 +12,16 @@ fn main() {
         .read_line(&mut master_pass)
         .expect("Could not read a line");
 
-    let mut keychain = KeyChain::new(master_pass.trim()).unwrap();
+    let keychain = Arc::new(Mutex::new(KeyChain::new(master_pass.trim()).unwrap()));
+    let k = keychain.clone();
+
+    ctrlc::set_handler(move || {
+        println!("Hello world!");
+        k.lock().unwrap().dump();
+        std::process::exit(128)
+    })
+    .unwrap();
+
     loop {
         println!("Enter service name or 'exit' to exit:");
         let mut input = String::new();
@@ -18,8 +30,8 @@ fn main() {
             .expect("Could not read a line");
         match input.trim() {
             "exit" => break,
-            _ => keychain.add_new_or_show_pass(input.trim()),
+            _ => keychain.lock().unwrap().add_new_or_show_pass(input.trim()),
         }
     }
-    keychain.dump();
+    keychain.lock().unwrap().dump();
 }
